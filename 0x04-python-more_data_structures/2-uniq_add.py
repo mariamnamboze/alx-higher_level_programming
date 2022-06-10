5eb7055c50af0dce1b421c62b59fa82b7b958a38
root <halimahmaria@>
2022-06-10 16:20:17 -0700

    Add all unique integers in a list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    return (set_1 & set_2)
+def uniq_add(my_list=[]):
+    result = 0
+    for x in set(my_list):
+        result += x
+    return (result)
